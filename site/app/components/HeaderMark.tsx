71ebab06bbe0d365fe1cba908d682bcf8cf31ad8
mateo zarate <mateozaratefw@gmail.com>
2026-09-09 13:07:34 -0300

    Use a transparent PNG for the homepage portrait

--- site/app/components/HeaderMark.tsx
+++ site/app/components/HeaderMark.tsx
@@ -117,12 +117,12 @@ export default function HeaderMark() {
   if (pathname === "/") {
     return (
       <Image
-        src="/portraits/mateo-stencil-detailed.png"
+        src="/portraits/mateo-stencil-transparent.png"
         alt="Stencil portrait of Mateo wearing a beret with a red star"
         width={1254}
         height={1254}
         sizes="(min-width: 640px) 96px, 80px"
-        className="h-auto w-20 shrink-0 mix-blend-multiply sm:w-24"
+        className="h-auto w-20 shrink-0 sm:w-24"
       />
     );
   }
